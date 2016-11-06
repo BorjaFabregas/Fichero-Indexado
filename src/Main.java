19f9182fa01f0e6ed736024b492a2808bdd27644
Borja Fabregas <borjafabregas.16@campuscamara.es>
2016-11-06 17:11:30 +0100

    Commit 17:11

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,40 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Administrador
- */
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
-        Fiche fichero = null;
+        Fiche fichero = new Fiche();
         String dni;
         int salario;
-        
-        fichero.crearfichero("Empleados");
-        
+
+        // fichero.crearfichero("empleados");
         do {
             do {
-                System.out.println("Elija una opcion");
-                System.out.println("1. dar de alta");
-                System.out.println("2. dar de baja");
-                System.out.println("3. modificar");
-                System.out.println("4. listar empleados, ordenado por DNI");
-                System.out.println("5. listar empleados, ordenados por orden de introduccion");
-                System.out.println("6. salir");
+                
+                System.out.println("1. Dar de alta");
+                System.out.println("2. Dar de baja");
+                System.out.println("3. Modificar");
+                System.out.println("4. Listar empleados, ordenado por DNI");
+                System.out.println("5. Listar empleados, ordenados por orden de introducción");
+                System.out.println("6. Salir");
+                System.out.print("Elija opción [1-6]: ");
                 opcion = Integer.parseInt(sc.nextLine());
             } while (opcion < 1 || opcion > 6);
 
             switch (opcion) {
                 case 1:
 
-                    insertar();
+                    fichero.alta(insertar());
+                    //fichero.muestraemple(insertar());
 
                     break;
                 case 2:
                     System.out.println("introduzca el dni que desea eliminar");
                     dni = sc.nextLine();
-                    fichero.baja(dni);
+                    // fichero.baja(dni);
                     break;
                 case 3:
                     System.out.println("introduzca el dni del empleado al que desea cambiar el salario ");
@@ -71,10 +64,26 @@ public class Main {
         String registros = "";
         String annioactual = Integer.toString(c.get(Calendar.YEAR));
         String empleado;
-        String nombreyape;
+        String nombreyape = "";;
         Empleado emple = null;
+        float salario = 0;
+        byte provincia = 0;
+        String provincias;
+        char sexo = 'f';
+        String dni = "";
+        String sexos;
+        String salarios;
+
+        String años;
+        int anio = 0;
+        String meses;
+        short mes = 0;
+        String dias;
+        short dia = 0;
+        String tipoemples;
+        char tipoemple = 'p';
         try {
-            String dni;
+
             //hay que controlar el dni que introduce que sea valido
             do {
                 System.out.print("Introduzca DNI: ");
@@ -86,7 +95,7 @@ public class Main {
             dni = registro.toString();
 
             do {
-                System.out.print("Introduzca el nombre y el apellido: [Máx 30 Caracteres]");
+                System.out.print("Introduzca Nombre y Apellidos: ");
                 nombreyape = sc.nextLine();
             } while (nombreyape.length() > 30);
             registro = new StringBuffer(nombreyape);
@@ -95,80 +104,63 @@ public class Main {
             //este es el nombre y apellidos del empleado que ocupa 30caracteres si o si
             empleado = registro.toString();
 
-            char sexo;
-            String sexos;
             do {
-                System.out.println("Introduzca M/H m:mujer h:hombre");
+                System.out.print("Introduzca M:Mujer H:Hombre: ");
                 sexos = sc.nextLine();
                 sexos = sexos.toUpperCase();
                 sexo = sexos.charAt(0);
             } while (sexo != 'M' && sexo != 'H');
 
-            float salario;
-            String salarios;
             do {
-                System.out.println("Introduzca el salario del empleado:");
+                System.out.print("Introduzca Salario: ");
                 salarios = sc.nextLine();
                 salario = Float.parseFloat(salarios);
             } while (salario <= 0 || salario >= 10000);
 
             //probablemente esto lo tengamos que cambiar pero de mientras lo voy a pillar como un int de 4
-            String años;
-            int anio;
-
             do {
-                System.out.println("Introduzca el año de contratacion del empleado:");
+                System.out.print("Introduzca Año de contratación: ");
                 años = sc.nextLine();
                 anio = Integer.parseInt(años);
                 //obviametne es con date y el 2016 seria fecha actual de hoy...
             } while (anio > Integer.parseInt(annioactual));
 
-            String meses;
-            short mes;
             do {
-                System.out.println("Introduzca el mes de contratacion  del empleado:");
+                System.out.print("Introduzca Mes de contratación: ");
                 meses = sc.nextLine();
                 mes = Short.parseShort(meses);
                 //obviametne es con date 
             } while (mes < 1 || mes > 12);
 
-            String dias;
-            short dia;
             do {
-                System.out.println("Introduzca el dia de contratacion del empleado:");
+                System.out.print("Introduzca Día de contratación: ");
                 dias = sc.nextLine();
                 dia = Short.parseShort(dias);
                 //obviametne es con date 
             } while (dia < 1 || dia > 31);
 
-            String tipoemples;
-            char tipoemple;
             do {
-                System.out.println("Introduzca el tipo de empleado:(C,F,D)");
+                System.out.print("Introduzca Tipo de empleado [C/D/F]: ");
                 tipoemples = sc.nextLine();
                 tipoemples = tipoemples.toUpperCase();
                 tipoemple = tipoemples.charAt(0);
-
             } while (tipoemple != 'C' && tipoemple != 'F' && tipoemple != 'D');
 
-            String provincias;
-            byte provincia;
             do {
-                System.out.println("Introduzca el numero de la provincia en la que trabaja");
+                System.out.print("Introduzca Número de Provincia [1-8]: ");
                 provincias = sc.nextLine();
                 provincia = Byte.parseByte(provincias);
-
             } while (provincia < 1 || provincia > 8);
 
             // do{
             //  }while(!"s".equals(continuar) && !"N".equals(continuar) && !"s".equals(continuar) && !"n".equals(continuar));
-            emple = new Empleado(nombreyape, sexo, salario, anio, mes, dia, tipoemple, provincia);
-
         } catch (NumberFormatException p) {
-            System.out.println(" ha habido algun fallo en la casteo de algun numero");
-        } finally {
-            return emple;
+            System.err.println("Error de Casteo");
         }
+
+        emple = new Empleado(nombreyape, sexo, salario, anio, mes, dia, tipoemple, provincia);
+
+        return emple;
 
     }
 
